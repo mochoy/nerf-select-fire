4081b3685f2f662e7c9f9d724e71944c1324581b
BobLakensi <montgomery.okdaily@gmail.com>
2017-08-13 14:03:35 -0700

    increment and decrement between firemodes based on joystick movement

--- nerf-select-fire.ino
+++ nerf-select-fire.ino
@@ -1,26 +1,68 @@
 #include <SoftwareSerial.h>
 
-#define JOY_INPUT_PIN 1
+//pins
+#define JOYSTICK_INPUT_PIN 1
+
+//"trip" values for joystick
+#define JOYSTICK_INCRECMENT_VAL 490
+#define JOYSTICK_DECREMENT_VAL 360
+
+//code for fire modes
+#define SAFETY 0
+#define SINGLE_FIRE 1
+#define BURST_FIRE 2
+#define FULL_AUTO 3
+
+//keep track of fire modes
+int fireMode = 0;   //0 = safe, 1 = single shot, 2 = burst, 3 = full auto
+
+//keep track of debouncing joystick
+int lastJoystickReading, debounceDelay = 50;
+double lastTime; 
 
 void setup () {
     Serial.begin(9600);
 }
 
-int treatValue(int data) {
-    return (data * 9 / 1024) + 48;
+void loop () {
+    toggleFireModes();
+    
+
 }
 
-void loop () {
-    int reading = (map(analogRead(JOY_INPUT_PIN), 0, 1023, 0, 500));
+//returns value based on up, down, or neutral
+//up = 0
+//neutral/middle = 1
+//down = 2
+int adjustedJoystickReading () {
+    int rawReading = (map(analogRead(JOYSTICK_INPUT_PIN), 0, 1023, 0, 500));
+
+    if (rawReading > JOYSTICK_INCRECMENT_VAL) {     //up
+        return 0;
+    } else if (rawReading < JOYSTICK_DECREMENT_VAL) {      //down
+        return 2;
+    } else {    //neutral
+        return 1;
+    }
+}
+
+void toggleFireModes () {
+    int joystickReading = adjustedJoystickReading();
 
 //    Serial.println(reading);
-    
-    if (reading > 490) {
-        Serial.println("up!");
-    } else if (reading < 360) {
-        Serial.println("down!");
-    }
+    if ((lastJoystickReading != joystickReading) && (millis() >= lastTime + debounceDelay)) {   //make sure joystick actually moved and check once every 50 milis
+        if (joystickReading == 0) {     //up
+            fireMode = ((fireMode == 3) ? 0 : fireMode + 1);    //increment fireMode
+        } else if (joystickReading == 2) {      //down
+            fireMode = ((fireMode == 0) ? 3 : fireMode - 1);    //decrement fireMode
 
-    delay(50);
+        }
+                
+        lastTime = millis();
+    }
+    
+    lastJoystickReading = joystickReading;
 }
+
+
 
