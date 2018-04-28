ecb6ecce2b78c5bbf3181ce923c65677499b9f23
Monty <etnom@users.noreply.github.com>
2018-04-28 12:05:31 -0700

    count darts on 2 conditions
    cc switch released if 0 darts shot, cc switch pressed if more than 1 dart shot

--- nerf-select-fire.ino
+++ nerf-select-fire.ino
@@ -83,7 +83,8 @@ void toggleFireModes () {
 //when dart fired
 void fire() {
   dartCountingSwitch.read();                                              //read button
-  if (dartCountingSwitch.wasReleased()) {
+  if ((dartCountingSwitch.wasReleased() && dartsFired == 0) ||
+    (dartsFired > 0 && dartCountingSwitch.wasPressed())) {
     dartsFired++;
     Serial.println("shot fired!");
   }
